ec389f98c7b7dba1da9b6f6289617261a9d9b8a4
Lulzim Morina <morinalulzim.fds@gmail.com>
2025-09-13 11:37:20 +0200

    Comments for better readability

--- calculator.py
+++ calculator.py
@@ -1,7 +1,12 @@
-# First Version of a Simple Calculator
+# Simple Calculator in Python - Version 1.0
+# Learning Goals: Variables, Input, Output, Basic Arithmetic
+
+# Greet the user
 print("Einfacher Taschenrechner")
 
+# Get two numbers from the user
 x = float(input("Gib die erste Zahl ein: "))
 y = float(input("Gib die zweite Zahl ein: "))
 
+# Perform addition and display the result
 print("Summe ist:", x + y)
