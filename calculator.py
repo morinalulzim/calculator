7707cec5a7dac835ba5a766ee336f2c347c4c444
Lulzim Morina <morinalulzim.fds@gmail.com>
2025-09-13 10:46:53 +0200

    first commit

---
+++ calculator.py
@@ -0,0 +1,7 @@
+# First Version of a Simple Calculator
+print("Einfacher Taschenrechner")
+
+x = float(input("Gib die erste Zahl ein: "))
+y = float(input("Gib die zweite Zahl ein: "))
+
+print("Summe ist:", x + y)
