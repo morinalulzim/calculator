2913867a1df138d0b106278fa1f2c9a20f643980
Lulzim Morina <morinalulzim.fds@gmail.com>
2025-09-15 09:56:31 +0200

    Refactor and clean up setuptools package
    - Removed unused script templates from setuptools.
    - Deleted unicode_utils.py as it was no longer needed.
    - Removed version.py which fetched setuptools version.
    - Eliminated wheel.py to streamline package management.
    - Deleted windows_support.py as it was specific to Windows functionality.
    - Cleaned up pyvenv.cfg configuration file.
    - Updated calculator_gui.py to improve UI with exit and resize functionality.

--- calculator_gui.py
+++ calculator_gui.py
@@ -1,16 +1,31 @@
 import tkinter
 
-# Funktion zu Schaltfläche Ender
-def ende():
+# Funktion zu Schaltfläche Exit
+def close_app():
     main.destroy()
+
+# Funktion zu Schaltfläche Resize
+def resize():
+    main.geometry("800x600")
 
 # Hauptfenster
 main = tkinter.Tk()
 main.title("Taschenrechner")
+main.geometry("600x400")
+main.configure(background="lightblue")
 
 # Schaltfläche Ende
-b = tkinter.Button(main, text="Ende", command=ende)
-b.pack()
+exitButton = tkinter.Button(main, text="Exit", command=close_app)
+exitButton["bg"] = "red"
+exitButton.pack()
+
+# Test Button
+resizeButton = tkinter.Button(main, text="Resize", command=resize)
+resizeButton["bg"] = "blue"
+resizeButton.pack()
+
+# Test Button für Reihen
+
 
 # Endlosschleife
 main.mainloop()
